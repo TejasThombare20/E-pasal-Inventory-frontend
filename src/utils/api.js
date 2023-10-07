a2b8fbccf0968c1e2abfa4b9654ecfdee14aa6f0
Tejas <thombaretejas@44gmail.com>
2023-10-07 19:05:18 +0530

    updaate .gitignore

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,8 @@
-const api = "https://e-pasal-inventory-backend.onrender.com";
-// const api = "http://localhost:5000";
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const api = process.env.REACT_APP_REDER_BACKEND_API;
+// const api = process.env.REACT_APP_LOCAL_BACKEND_API;
 // const api = "https://e-pasal-inventory-backend.vercel.app/";
 module.exports  = api;
